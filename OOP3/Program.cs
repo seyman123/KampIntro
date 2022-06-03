6c30a5bff3f7aa380aea8d2aa0b13394ba6aafd6
ibrahim seyman <seyman12345@gmail.com>
2022-06-04 00:11:57 +0300

    04.06.22

---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers1 = new List<ILoggerService>() { databaseLoggerService, fileLoggerService };
+            List<ILoggerService> loggers2 = new List<ILoggerService>() { fileLoggerService, smsLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers1);
+            basvuruManager.BasvuruYap(konutKrediManager, loggers2);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { tasitKrediManager , ihtiyacKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
